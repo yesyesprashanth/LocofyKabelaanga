b9ec1f2b7bc2ae4945fd931f2c6301f0fd238619
Prashanth <yesyesprashanth@gmail.com>
2023-06-24 10:18:19 +0530

    Responsive and better

--- src/components/StrangeWallContainer.js
+++ src/components/StrangeWallContainer.js
@@ -2,18 +2,8 @@ import ThrillerCardContainer from "./ThrillerCardContainer";
 import styles from "./StrangeWallContainer.module.css";
 const StrangeWallContainer = () => {
   return (
-    <div className={styles.cardllist}>
-      <section className={styles.card} id="card">
-        <img className={styles.cardPicIcon} alt="" src="/card-pic@2x.png" />
-        <div className={styles.cardName}>
-          <div className={styles.genre}>
-            <b className={styles.thriller}>THRILLER</b>
-          </div>
-          <div className={styles.title}>
-            <b className={styles.thriller}>Strange Wall</b>
-          </div>
-        </div>
-      </section>
+    <div className={styles.cardllist}>    
+      <ThrillerCardContainer imageIds="/card-pic1@2x.png" />
       <ThrillerCardContainer imageIds="/card-pic1@2x.png" />
       <ThrillerCardContainer imageIds="/card-pic@2x.png" />
       <ThrillerCardContainer imageIds="/card-pic@2x.png" />
